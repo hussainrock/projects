bdd93e3a401d32dea99fad15c2b89430d2b54915
hussainrock <119557797+hussainrock@users.noreply.github.com>
2023-04-07 17:53:35 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print('hello')
+print('my name is hussain cool')
+
+
+# In[ ]:
+
+
+print('hello')
+
+
+# In[ ]:
+
+
+
+
+
+# # variables
+
+# In[1]:
+
+
+print('hello')
+
